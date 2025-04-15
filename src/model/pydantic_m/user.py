5bdf49e139a7a82bf5815d0228fa2b9b74c28f26
xunyang <xunyang@deloitte.com.cn>
2025-04-15 10:04:32 +0800

    fix:用户创建优化

--- src/model/pydantic_m/user.py
+++ src/model/pydantic_m/user.py
@@ -1,6 +1,20 @@
-from pydantic import BaseModel, Field
+from enum import Enum
+
+from pydantic import BaseModel, Field, field_validator
+
+
+class UserStatus(Enum):
+    DISABLED = 0  # 禁用
+    ENABLED = 1  # 启用
 
 
 class UserParam(BaseModel):
-    user_name: str = Field(default="yxy", description="用户名称")
-    user_status: int = Field(default=1, description="用户状态")
+    user_name: str = Field(default="yxy", description="用户名称", min_length=3, max_length=20)
+    user_status: int = Field(default=UserStatus.ENABLED.value, description="用户状态")
+
+    @field_validator("user_status")
+    def validate_user_status(cls, value):
+        valid_values = [status.value for status in UserStatus]
+        if value not in valid_values:
+            raise ValueError(f"user_status 必须是 {valid_values} 中的一个")
+        return value
