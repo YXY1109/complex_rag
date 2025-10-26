978ae40383489678c7c8195394952ac9d73b74f3
YXY1109 <1049598673@qq.com>
2025-10-26 11:25:57 +0800

    update：清理原来的代码目录文件

--- src/model/pydantic_m/knowledge.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class KnowledgeParam(BaseModel):
-    user_id: int = Field(default=1, description="用户id")
-    p_name: str = Field(default="民法典", description="知识库名称")
